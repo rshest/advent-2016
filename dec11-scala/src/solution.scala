c25b6bf9069f745e7213200b3f2778119a500e93
Ruslan Shestopalyuk <rushbits@gmail.com>
2016-12-20 09:26:38 +0100

    resuming the work

--- dec11-scala/src/solution.scala
+++ dec11-scala/src/solution.scala
@@ -6,25 +6,29 @@ object Solution {
   case object Generator extends ElementType
   case object Microchip extends ElementType
 
-  class Element(val caption: String, val kind: ElementType, var currentFloor: Int) {
+  class Element(val caption: String, val kind: ElementType, var floor: Int) {
+    override def toString(): String =
+      s"${caption.toUpperCase.slice(0, 2)}${kind.toString.toUpperCase.head}[${floor}]"
   }
 
-  def parse(lines: Array[String]): Array[Element] = {
-
+  def parseElements(lines: Seq[String]): Array[Element] = {
+    val pattern = "([^\\s]+) generator|([^\\s]+)-compatible microchip".r
+    lines.zipWithIndex.flatMap { case (line, floor) =>
+      pattern.findAllIn(line).matchData map { m =>
+        val (n, kind) = if (m.group(2) == null) (1, Generator) else (2, Microchip)
+        new Element(m.group(n), kind, floor + 1)
+      }
+    }.toArray
   }
-
 
   def main(args: Array[String]): Unit = {
     val file = if (args.length > 0) args(0) else "test.txt"
     val lines = Source.fromFile(file).getLines.toList
 
-    val pattern = "([^\\s]+) generator|([^\\s]+)-compatible microchip".r
+    val elems = parseElements(lines)
 
-    for (line <- lines) {
-      pattern.findAllIn(line).matchData foreach {
-        m => println(s"G: ${m.group(1)}, M: ${m.group(2)}")
-      }
-      println("---")
+    for (el <- elems) {
+      println(el.toString)
     }
   }
 }
