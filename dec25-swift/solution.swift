d5aec27f8699c8eecbd765614d5ba411b2c20219
Ruslan Shestopalyuk <rushbits@gmail.com>
2017-01-02 15:20:20 +0100

    - swift wip

--- dec25-swift/solution.swift
+++ dec25-swift/solution.swift
@@ -1,4 +1,12 @@
-let myAwesomeString = "hey I am an awesome string"
-print(myAwesomeString)
-let awesomeInt = 500
-print(awesomeInt)
+
+import Foundation
+
+
+
+let args = CommandLine.arguments
+let fname = args.count > 1 ? args[1] : "input.txt"
+
+let content = try String(contentsOfFile: fname, encoding: String.Encoding.ascii)
+let lines = content.components(separatedBy:"\n")
+
+print(lines)
