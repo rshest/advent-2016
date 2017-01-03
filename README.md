1085a7e009ab9686ac799694bc70a113dc6c8d61
Ruslan Shestopalyuk <rushbits@gmail.com>
2017-01-03 20:46:00 +0100

    Day 22 - Rust

--- README.md
+++ README.md
@@ -3,18 +3,18 @@ My solutions to [Advent of Code 2016](http://adventofcode.com/2016) problems.
 
 The goal is to use a different programming language for every problem:
 
-1. Python
+1. [Day 01](http://adventofcode.com/2016/day/1):  Python
     ```bash
     $ python solution.py < input.txt
     ```
 
-2. C++
+2. [Day 02](http://adventofcode.com/2016/day/2): C++
     ```bash
     $ g++ -std=c++11 solution.cpp -o solution
     $ ./solution < input.txt
     ```
 
-3. Factor
+3. [Day 03](http://adventofcode.com/2016/day/3): Factor
 
     In the listener:
     ```bash
@@ -23,120 +23,119 @@ The goal is to use a different programming language for every problem:
     IN: scratchpad main
     ```
 
-4. Nim
+4. [Day 04](http://adventofcode.com/2016/day/4): Nim
     ```bash
     $ nim c --opt:speed solution.nim && ./solution < input.txt
     ```
 
-5. TypeScript
+5. [Day 05](http://adventofcode.com/2016/day/5): TypeScript
     ```bash
     $ npm install @types/node
-    $ npm install crypto
+    $ npm install crypto``
     $ tsc --lib es6 solution.ts && node solution.js < input.txt
     ```
 
-6. Julia
+6. [Day 06](http://adventofcode.com/2016/day/6): Julia
     ```bash
     $ julia solution.jl input.txt
     ```
 
-7. F#
+7. [Day 07](http://adventofcode.com/2016/day/7): F#
     ```bash
     $ fsi solution.fsx input.txt
     ```
 
-8. Lua
+8. [Day 08](http://adventofcode.com/2016/day/8): Lua
     ```bash
     $ lua5.1 solution.lua input.txt
     ```
 
-9. Kotlin
+9. [Day 09](http://adventofcode.com/2016/day/9): Kotlin
     ```bash
     $ kotlinc -script solution.kts input.txt
     ```
 
-10. Elm
+10. [Day 10](http://adventofcode.com/2016/day/10): Elm
     ```bash
     $ elm-reactor
     ```
     Then, open http://localhost:8000/solution.elm in a browser.
 
-11. Scala
+11. [Day 11](http://adventofcode.com/2016/day/11): Scala
     ```bash
     $ scala solution.scala input1.txt
     $ scala -J"-Xmx2048m" solution.scala input2.txt
     ```
 
-12. Dart
+12. [Day 12](http://adventofcode.com/2016/day/12): Dart
     ```bash
     $ dart solution.dart input.txt
     ```
 
-13. Java
+13. [Day 13](http://adventofcode.com/2016/day/13): Java
     ```bash
     $ cd src && javac solution.java && java solution ../input.txt
     ```
 
-14. Ruby
+14. [Day 14](http://adventofcode.com/2016/day/14): Ruby
     ```bash
     $ ruby solution.rb input.txt
     ```
 
-15. Haskell
+15. [Day 15](http://adventofcode.com/2016/day/15): Haskell
     ```bash
     $ ghc solution.hs && ./solution input1.txt && ./solution input2.txt
     ```
 
-16. Go
+16. [Day 16](http://adventofcode.com/2016/day/16): Go
     ```bash
     $ go run solution.go input1.txt
     $ go run solution.go input2.txt
     ```
 
-17. Clojure
+17. [Day 17](http://adventofcode.com/2016/day/17): Clojure
     ```bash
     $ lein run input.txt
     ```
 
-18. Elixir
+18. [Day 18](http://adventofcode.com/2016/day/18): Elixir
     ```bash
     $ elixir solution.exs input.txt 40
     $ elixir solution.exs input.txt 400000
     ```
 
-19. Crystal
+19. [Day 19](http://adventofcode.com/2016/day/19): Crystal
     ```bash
     $ crystal solution.cr
     ```
 
-20. C#
+20. [Day 20](http://adventofcode.com/2016/day/20): C#
      ```bash
      $ mcs Solution.cs && mono Solution.exe
      ```
 
-21. Ada
+21. [Day 21](http://adventofcode.com/2016/day/21): Ada
     ```bash
     $ gnatmake -gnata solution.adb && ./solution
     ```
 
-22. Rust
+22. [Day 22](http://adventofcode.com/2016/day/22): Rust
      ```bash
-     $ rustc solution.rs && ./solution input.txt
+     $ cargo run --release solution.rs input.txt
      ```
 
-22. D
+23. [Day 23](http://adventofcode.com/2016/day/23): D
     ```bash
     $ rdmd -release solution input.txt 7
     $ rdmd -release solution input.txt 12
     ```
 
-
-24. Ceylon
+24. [Day 24](http://adventofcode.com/2016/day/24): Ceylon
     ```bash    
     $ ceylon run --compile=force solution
     ```
 
-25. Swift
+25. [Day 25](http://adventofcode.com/2016/day/25): Swift
     ```bash    
     $ swift solution.swift input.txt
     ```
--- dec22-rust/solution.rs
+++ dec22-rust/solution.rs
@@ -1,14 +1,187 @@
 use std::env;
+use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::Read;
+use std::hash::{Hash, Hasher};
+
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+type Coord = (i32, i32);
+
+#[derive(Debug)]
+struct Node {
+  pub pos : Coord,
+  pub size : u32,
+  pub used : u32,
+  pub avail : u32,
+  pub use_percent : u32
+}
+
+const NODE_REGEX: &'static str =
+r".*node-x(\d+)-y(\d+)\D+(\d+)T+\D+(\d+)T+\D+(\d+)T+\D+(\d+)%";
+
+fn parse_node(str: &str) -> Option<Node> {
+  lazy_static! {
+    static ref RE: Regex = Regex::new(NODE_REGEX).unwrap();
+  }
+
+  if RE.is_match(str) {
+    let cap = RE.captures(str).unwrap();
+    let part = |i: usize| cap[i + 1].parse::<u32>().unwrap();
+    Some(Node {pos : (part(0) as i32, part(1) as i32),
+      size : part(2), used : part(3), avail : part(4), use_percent : part(5)
+    })
+  } else {
+    None
+  }
+}
+
+fn is_viable_pair(a: &Node, b: &Node) -> bool {
+  a.used > 0 && a.pos != b.pos && a.used <= b.avail
+}
+
+fn count_viable_pairs(nodes: &Vec<Node>) -> u32 {
+  let mut res = 0;
+  let n = nodes.len();
+  for i in 0..n {
+    for j in 0..n {
+      if is_viable_pair(&nodes[i], &nodes[j]) {
+        res += 1;
+      }
+    }
+  }
+  res
+}
+
+fn find_extents(nodes: &Vec<Node>) -> Coord {
+  (nodes.iter().map(|n| n.pos.0).max().unwrap() + 1,
+   nodes.iter().map(|n| n.pos.1).max().unwrap() + 1)
+}
+
+fn find_blank(nodes: &Vec<Node>) -> Coord {
+  nodes.iter().find(|n| n.used == 0).unwrap().pos
+}
+
+fn find_path(nodes: &Vec<Node>, from: Coord, to: Coord) -> Vec<Coord> {
+  #[derive(Copy, Clone)]
+  struct State {
+    cost: u32,
+    blank_pos: Coord,
+    data_pos: Coord,
+    prev: i32,
+    index: i32
+  }
+
+  impl Ord for State {
+    fn cmp(&self, other: &State) -> Ordering {
+      other.cost.cmp(&self.cost)
+    }
+  }
+
+  impl PartialOrd for State {
+    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+      Some(self.cmp(other))
+    }
+  }
+
+  impl  PartialEq for State {
+    fn eq(&self, other: &State) -> bool {
+      self.blank_pos == other.blank_pos && self.data_pos == other.data_pos
+    }
+  }
+
+  impl Eq for State {}
+
+  impl Hash for State {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+      self.blank_pos.hash(state);
+      self.data_pos.hash(state);
+    }
+  }
+
+  let start_state = State {
+    cost: 0,
+    blank_pos: find_blank(nodes),
+    data_pos: from,
+    prev: -1,
+    index: 0
+  };
+
+  let node_reg: HashMap<Coord, &Node> = nodes.iter().map(|n| (n.pos.clone(), n)).collect();
+
+  let mut states = Vec::new();
+  let mut open = BinaryHeap::new();
+  let mut closed = HashSet::new();
+
+  open.push(start_state.clone());
+  closed.insert(start_state.clone());
+  states.push(start_state);
+
+  while !open.is_empty() {
+    let state = open.pop().unwrap();
+    let node = node_reg.get(&state.blank_pos).unwrap();
+
+    if state.data_pos == to {
+      //  reached the target
+      let mut cs = &state;
+      let mut res = Vec::new();
+      while cs.prev != -1 {
+        res.push(cs.blank_pos.clone());
+        cs = &states[cs.prev as usize];
+      }
+      return res;
+    }
+
+    for offs in [(-1, 0), (0, -1), (1, 0), (0, 1)].iter() {
+      let pos = (state.blank_pos.0 + offs.0, state.blank_pos.1 + offs.1);
+      let new_node = match node_reg.get(&pos) {
+        Some(neighbor) if neighbor.used <= node.size => neighbor,
+        _ => continue
+      };
+
+      let new_idx = states.len() as i32;
+      let new_state = State {
+        cost: state.cost + 1,
+        blank_pos: new_node.pos,
+        data_pos: if state.data_pos == pos { state.blank_pos } else { state.data_pos },
+        prev: state.index,
+        index: new_idx
+      };
+
+      let better_state = match closed.get(&new_state) {
+        Some(vs) => vs.cost > new_state.cost,
+        None => true
+      };
+
+      if better_state {
+        open.push(new_state.clone());
+        closed.insert(new_state.clone());
+      }
+      states.push(new_state.to_owned());
+    }
+  }
+  Vec::new()
+}
 
 fn main() {
   let args: Vec<_> = env::args().collect();
-  let fname = if args.len() > 1 { &args[1] } else { "input.txt" };
+  let fname = if args.len() > 2 { &args[2] } else { "input.txt" };
 
   let mut file = File::open(fname).unwrap();
   let mut input = String::new();
   file.read_to_string(&mut input).unwrap();
 
-  println!("{}", seed);
+  let nodes: Vec<Node> =
+    input.split("\n").map(|s| parse_node(s)).filter_map(|x| x).collect();
+
+  println!("Number of viable pairs: {}", count_viable_pairs(&nodes));
+
+  let (w, _) = find_extents(&nodes);
+  let path = find_path(&nodes, (w - 1, 0), (0, 0));
+  println!("Shortest from top-right to top-left: {}", path.len());
 }
